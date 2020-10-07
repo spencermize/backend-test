e043715e37eaf2e10247e242d95aa8152c020055
spencermize <spence.bassman@gmail.com>
2020-10-06 23:54:22 -0400

    create an admin editing experience

---
+++ app/src/AuthorAdmin.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Vendor\Booker;
+use SilverStripe\Admin\ModelAdmin;
+
+class AuthorAdmin extends ModelAdmin {
+
+	private static $managed_models = [
+		Author::class
+	];
+
+	private static $url_segment = 'authors';
+	private static $menu_title = 'Authors';
+	private static $menu_icon_class = 'font-icon-torsos-all';
+}
---
+++ app/src/BookAdmin.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Vendor\Booker;
+use SilverStripe\Admin\ModelAdmin;
+
+class BookAdmin extends ModelAdmin {
+
+	private static $managed_models = [
+		Book::class
+	];
+
+	private static $url_segment = 'books';
+	private static $menu_title = 'Books';
+	private static $menu_icon_class = 'font-icon-book';
+}
