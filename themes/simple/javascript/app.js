eda7625fe5b02464b56272af4f213cca0cfc1751
spencermize <spence.bassman@gmail.com>
2020-10-07 22:32:02 -0400

    check in the build files for simplicity...

---
+++ themes/simple/javascript/app.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{29:function(n,a,e){e(30),n.exports=e(35)},30:function(n,a,e){"use strict";e.r(a);var o=e(19),t=e.n(o),u=e(28),p=e(24);t.a.use(u.a),t.a.use(p.a),new t.a({el:"#app",data:function(){return{name:"Booker"}}})},35:function(n,a){}},[[29,1,2]]]);
