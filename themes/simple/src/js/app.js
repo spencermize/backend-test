90d4cd78d7d533661bf80fac3403626561c34d69
spencermize <spence.bassman@gmail.com>
2020-10-10 17:25:38 -0400

    set up Vue routes and a central data store

--- themes/simple/src/js/app.js
+++ themes/simple/src/js/app.js
@@ -1,26 +1,55 @@
+/* global GOOGLE_API */
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Lists from '../vue/Lists.vue';
+import SingleList from '../vue/SingleList.vue';
+import SingleBook from '../vue/SingleBook.vue';
+import mixins from './mixins.js';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
+Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
 // Now, on to the show!
 if (!window.location.href.includes('/Security/login')) {
-	// We *should* have a PHP session (required to establish ownership of BookLists), because HomePage requires login.
+	// We *should* have a PHP session (required to establish ownership of BookLists), because HomePage requires login
+	window.store = {
+		state: {
+			loading: false,
+			activeList: false,			
+			error: false,
+			nav: [
+				{
+					text: 'Home',
+					to: { path: '/' }
+				}
+			],			
+		},
+		lists: [],
+		bookList: {},		
 	};
 
+	const routes = [
+		{ path: '/', component: Lists },
+		{ path: '/lists/:id', component: SingleList },
+		{ path: '/books/:id', component: SingleBook }
+	];
+	const router = new VueRouter({
+		routes
 	});
 
 	new Vue({
 		el: '#app',
+		router,
+		mixins: [mixins],
 		data() {
 			return {
-				name: 'Booker',
-				lists: []
+
 			};
 		},
 		async mounted() {
-			this.lists = await this.ajaxRequest(routes.BookList);
+			this.store.lists = await this.ajaxRequest(this.routes.BookList);
 		},
 		methods: {
 
