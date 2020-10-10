35f23d7bd7dc8962dfe0930b9673fe09ef5b4307
spencermize <spence.bassman@gmail.com>
2020-10-10 17:24:39 -0400

    this stuff was abstracted to the helper mixin

--- themes/simple/src/js/app.js
+++ themes/simple/src/js/app.js
@@ -7,15 +7,8 @@ Vue.use(IconsPlugin);
 // Now, on to the show!
 if (!window.location.href.includes('/Security/login')) {
 	// We *should* have a PHP session (required to establish ownership of BookLists), because HomePage requires login.
-	const namespace = '/api/v1/Spencer-Booker-';
-	const routes = {
-		Author: 'Author',
-		Book: 'Book',
-		BookList: 'BookList',
 	};
 
-	Object.keys(routes).forEach(route => {
-		routes[route] = namespace + routes[route];
 	});
 
 	new Vue({
@@ -30,26 +23,7 @@ if (!window.location.href.includes('/Security/login')) {
 			this.lists = await this.ajaxRequest(routes.BookList);
 		},
 		methods: {
-			async ajaxRequest(route, method, params) {
-				const headers = new Headers();
-				headers.append('Accept', 'application/json');
 
-				const options = {
-					method: method || 'GET',
-					headers
-				};
-
-				try {
-					const resp = await fetch(`${route}?${params || ''}`, options);
-					if (resp.status === 200) {
-						return await resp.json();
-					} else {
-						throw Error(resp.statusText);
-					}
-				} catch (err) {
-					console.log(err);
-				}
-			}
 		}
 	});
 }
