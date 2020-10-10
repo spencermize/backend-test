1b83f673567680b672bd0f13179b586ba6ceef9e
spencermize <spence.bassman@gmail.com>
2020-10-10 17:41:04 -0400

    don't extract vue styles

--- themes/simple/webpack.mix.js
+++ themes/simple/webpack.mix.js
@@ -3,10 +3,6 @@ const webpack = require('webpack');
 const mix = require('laravel-mix');
 require('dotenv').config();
 
-mix.options({
-	extractVueStyles: 'dist/vue.css'
-});
-
 mix
 	.sass('src/scss/app.scss', 'css')
 	.js('src/js/app.js', 'javascript')
